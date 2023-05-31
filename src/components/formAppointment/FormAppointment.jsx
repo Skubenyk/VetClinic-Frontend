61d1a56eadbf8cec1b7c1de3af5014e092b1d83b
Skubenyk <skubenikl@gmail.com>
2023-05-31 13:39:47 +0300

    Add Form of sending by email

--- src/components/formAppointment/FormAppointment.jsx
+++ src/components/formAppointment/FormAppointment.jsx
@@ -1,34 +1,167 @@
-//*FormAppointment
+// //*FormAppointment
 
-// *Import base
+// // *Import base
+// import styles from './formAppointment.module.scss';
+// import classNames from 'classnames';
+
+// // *Import Formik libraries for creating forms
+// import { useFormik } from 'formik';
+
+// // *Import Yup libraries for validation forms
+// import * as Yup from 'yup';
+
+// const FormAppointment = ({ active, setActive }) => {
+//   //*Formik
+//   const formik = useFormik({
+//     initialValues: {
+//       name: '',
+//       tel: '',
+//       text: '',
+//     },
+//     //*Validation
+//     validationSchema: Yup.object({
+//       name: Yup.string()
+//         .min(2, 'Минимум 2 символова')
+//         .required('Обязательное поле!'),
+//       tel: Yup.number()
+//         .typeError('Неправильный номер телефона')
+//         .min(10, 'Минимум  10 символов')
+//         .required('Обязательное поле!'),
+//       text: Yup.string()
+//         .min(30, 'Минимум  30 символов')
+//         .required('Обязательное поле!'),
+//     }),
+//     onSubmit: (values, { setSubmitting, resetForm }) => {
+//       console.log(JSON.stringify(values, null, 2));
+//       setTimeout(() => {
+//         setSubmitting(false);
+//         resetForm();
+//       }, 400);
+//     },
+//   });
+
+//   return (
+//     <div
+//       //*Сonditions for switching styles in mode active
+//       className={
+//         active ? classNames(styles.modal, styles.active) : styles.modal
+//       }
+//       onClick={() => setActive(false)}
+//     >
+//       {/* Form */}
+//       <div className={styles.form}>
+//         <div className={styles.formWrapper}>
+//           <form
+//             action='#'
+//             method='post'
+//             onSubmit={formik.handleSubmit}
+//             data-netlify='true'
+//           >
+//             <div className={styles.conntainerForm}>
+//               {/* Title form */}
+//               <h2 className={styles.titleForm}>Запись на прием</h2>
+//               <div className={styles.formContent}>
+//                 {/* Name */}
+//                 <label htmlFor='name'></label>
+//                 <input
+//                   className={styles.name}
+//                   type='text'
+//                   placeholder='Имя'
+//                   name='name'
+//                   value={formik.values.name}
+//                   onChange={formik.handleChange}
+//                   onBlur={formik.handleBlur}
+//                   onClick={(e) => e.stopPropagation()}
+//                 />
+//                 {/* Conditions for including a style error */}
+//                 {formik.errors.name && formik.touched.name ? (
+//                   <div className={styles.error}>{formik.errors.name}</div>
+//                 ) : null}
+//                 {/* Telephone */}
+//                 <label htmlFor='tel'></label>
+//                 <input
+//                   className={styles.tel}
+//                   type='tel'
+//                   placeholder='Телефон'
+//                   name='tel'
+//                   value={formik.values.tel}
+//                   onChange={formik.handleChange}
+//                   onBlur={formik.handleBlur}
+//                   onClick={(e) => e.stopPropagation()}
+//                 />
+//                 {/* Conditions for including a style error */}
+//                 {formik.errors.tel && formik.touched.tel ? (
+//                   <div className={styles.error}>{formik.errors.tel}</div>
+//                 ) : null}
+//                 {/* Text */}
+//                 <label htmlFor='text'></label>
+//                 <textarea
+//                   className={styles.formContentText}
+//                   rows='8'
+//                   placeholder='Кратко опишите проблему'
+//                   name='text'
+//                   value={formik.values.text}
+//                   onChange={formik.handleChange}
+//                   onBlur={formik.handleBlur}
+//                   onClick={(e) => e.stopPropagation()}
+//                 />
+//                 {/* Conditions for including a style error */}
+//                 {formik.errors.text && formik.touched.text ? (
+//                   <div className={styles.error}>{formik.errors.text}</div>
+//                 ) : null}
+//               </div>
+//               {/* Buttons */}
+//               <div className={styles.formButton}>
+//                 <button
+//                   className={styles.buttonSubmit}
+//                   type='submit'
+//                   onClick={(e) => e.stopPropagation()}
+//                 >
+//                   <span>ОТПРАВИТЬ</span>
+//                 </button>
+//                 <button
+//                   //*Close form
+//                   className={styles.buttonClose}
+//                   type='button'
+//                 >
+//                   <span>ЗАКРЫТЬ</span>
+//                 </button>
+//               </div>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FormAppointment;
+
+// ! Компонент з відправкою даних форми на пошту
+import React from 'react';
 import styles from './formAppointment.module.scss';
 import classNames from 'classnames';
-
-// *Import Formik libraries for creating forms
 import { useFormik } from 'formik';
-
-// *Import Yup libraries for validation forms
 import * as Yup from 'yup';
 
 const FormAppointment = ({ active, setActive }) => {
-  //*Formik
+  const URL = 'https://vetclinic-backend.onrender.com/api/users/send-email';
   const formik = useFormik({
     initialValues: {
       name: '',
       tel: '',
       text: '',
     },
-    //*Validation
     validationSchema: Yup.object({
       name: Yup.string()
-        .min(2, 'Минимум 2 символова')
+        .min(2, 'Минимум 2 символа')
         .required('Обязательное поле!'),
-      tel: Yup.number()
-        .typeError('Неправильный номер телефона')
-        .min(10, 'Минимум  10 символов')
+      tel: Yup.string()
+        .matches(/^\d+$/, 'Неправильный номер телефона')
+        .min(10, 'Минимум 10 символов')
         .required('Обязательное поле!'),
       text: Yup.string()
-        .min(30, 'Минимум  30 символов')
+        .min(30, 'Минимум 30 символов')
         .required('Обязательное поле!'),
     }),
     onSubmit: (values, { setSubmitting, resetForm }) => {
@@ -37,31 +170,39 @@ const FormAppointment = ({ active, setActive }) => {
         setSubmitting(false);
         resetForm();
       }, 400);
+      sendData(values);
     },
   });
 
+  const sendData = async (formData) => {
+    try {
+      const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div
-      //*Сonditions for switching styles in mode active
       className={
         active ? classNames(styles.modal, styles.active) : styles.modal
       }
       onClick={() => setActive(false)}
     >
-      {/* Form */}
       <div className={styles.form}>
         <div className={styles.formWrapper}>
-          <form
-            action='#'
-            method='post'
-            onSubmit={formik.handleSubmit}
-            data-netlify='true'
-          >
+          <form action='#' method='post' onSubmit={formik.handleSubmit}>
             <div className={styles.conntainerForm}>
-              {/* Title form */}
               <h2 className={styles.titleForm}>Запись на прием</h2>
               <div className={styles.formContent}>
-                {/* Name */}
                 <label htmlFor='name'></label>
                 <input
                   className={styles.name}
@@ -73,11 +214,9 @@ const FormAppointment = ({ active, setActive }) => {
                   onBlur={formik.handleBlur}
                   onClick={(e) => e.stopPropagation()}
                 />
-                {/* Conditions for including a style error */}
                 {formik.errors.name && formik.touched.name ? (
                   <div className={styles.error}>{formik.errors.name}</div>
                 ) : null}
-                {/* Telephone */}
                 <label htmlFor='tel'></label>
                 <input
                   className={styles.tel}
@@ -89,11 +228,9 @@ const FormAppointment = ({ active, setActive }) => {
                   onBlur={formik.handleBlur}
                   onClick={(e) => e.stopPropagation()}
                 />
-                {/* Conditions for including a style error */}
                 {formik.errors.tel && formik.touched.tel ? (
                   <div className={styles.error}>{formik.errors.tel}</div>
                 ) : null}
-                {/* Text */}
                 <label htmlFor='text'></label>
                 <textarea
                   className={styles.formContentText}
@@ -105,12 +242,10 @@ const FormAppointment = ({ active, setActive }) => {
                   onBlur={formik.handleBlur}
                   onClick={(e) => e.stopPropagation()}
                 />
-                {/* Conditions for including a style error */}
                 {formik.errors.text && formik.touched.text ? (
                   <div className={styles.error}>{formik.errors.text}</div>
                 ) : null}
               </div>
-              {/* Buttons */}
               <div className={styles.formButton}>
                 <button
                   className={styles.buttonSubmit}
@@ -119,11 +254,7 @@ const FormAppointment = ({ active, setActive }) => {
                 >
                   <span>ОТПРАВИТЬ</span>
                 </button>
-                <button
-                  //*Close form
-                  className={styles.buttonClose}
-                  type='button'
-                >
+                <button className={styles.buttonClose} type='button'>
                   <span>ЗАКРЫТЬ</span>
                 </button>
               </div>
