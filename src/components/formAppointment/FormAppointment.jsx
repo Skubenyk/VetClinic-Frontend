e08587601550e2f76dbce1e1ac620a9d04a040d1
Skubenyk <skubenikl@gmail.com>
2023-06-01 12:24:41 +0300

    Replacing the fetch method with axios when sending data from all forms

--- src/components/formAppointment/FormAppointment.jsx
+++ src/components/formAppointment/FormAppointment.jsx
@@ -1,148 +1,14 @@
-// //*FormAppointment
+//*FormAppointment
 
-// // *Import base
-// import styles from './formAppointment.module.scss';
-// import classNames from 'classnames';
-
-// // *Import Formik libraries for creating forms
-// import { useFormik } from 'formik';
-
-// // *Import Yup libraries for validation forms
-// import * as Yup from 'yup';
-
-// const FormAppointment = ({ active, setActive }) => {
-//   //*Formik
-//   const formik = useFormik({
-//     initialValues: {
-//       name: '',
-//       tel: '',
-//       text: '',
-//     },
-//     //*Validation
-//     validationSchema: Yup.object({
-//       name: Yup.string()
-//         .min(2, 'Минимум 2 символова')
-//         .required('Обязательное поле!'),
-//       tel: Yup.number()
-//         .typeError('Неправильный номер телефона')
-//         .min(10, 'Минимум  10 символов')
-//         .required('Обязательное поле!'),
-//       text: Yup.string()
-//         .min(30, 'Минимум  30 символов')
-//         .required('Обязательное поле!'),
-//     }),
-//     onSubmit: (values, { setSubmitting, resetForm }) => {
-//       console.log(JSON.stringify(values, null, 2));
-//       setTimeout(() => {
-//         setSubmitting(false);
-//         resetForm();
-//       }, 400);
-//     },
-//   });
-
-//   return (
-//     <div
-//       //*Сonditions for switching styles in mode active
-//       className={
-//         active ? classNames(styles.modal, styles.active) : styles.modal
-//       }
-//       onClick={() => setActive(false)}
-//     >
-//       {/* Form */}
-//       <div className={styles.form}>
-//         <div className={styles.formWrapper}>
-//           <form
-//             action='#'
-//             method='post'
-//             onSubmit={formik.handleSubmit}
-//             data-netlify='true'
-//           >
-//             <div className={styles.conntainerForm}>
-//               {/* Title form */}
-//               <h2 className={styles.titleForm}>Запись на прием</h2>
-//               <div className={styles.formContent}>
-//                 {/* Name */}
-//                 <label htmlFor='name'></label>
-//                 <input
-//                   className={styles.name}
-//                   type='text'
-//                   placeholder='Имя'
-//                   name='name'
-//                   value={formik.values.name}
-//                   onChange={formik.handleChange}
-//                   onBlur={formik.handleBlur}
-//                   onClick={(e) => e.stopPropagation()}
-//                 />
-//                 {/* Conditions for including a style error */}
-//                 {formik.errors.name && formik.touched.name ? (
-//                   <div className={styles.error}>{formik.errors.name}</div>
-//                 ) : null}
-//                 {/* Telephone */}
-//                 <label htmlFor='tel'></label>
-//                 <input
-//                   className={styles.tel}
-//                   type='tel'
-//                   placeholder='Телефон'
-//                   name='tel'
-//                   value={formik.values.tel}
-//                   onChange={formik.handleChange}
-//                   onBlur={formik.handleBlur}
-//                   onClick={(e) => e.stopPropagation()}
-//                 />
-//                 {/* Conditions for including a style error */}
-//                 {formik.errors.tel && formik.touched.tel ? (
-//                   <div className={styles.error}>{formik.errors.tel}</div>
-//                 ) : null}
-//                 {/* Text */}
-//                 <label htmlFor='text'></label>
-//                 <textarea
-//                   className={styles.formContentText}
-//                   rows='8'
-//                   placeholder='Кратко опишите проблему'
-//                   name='text'
-//                   value={formik.values.text}
-//                   onChange={formik.handleChange}
-//                   onBlur={formik.handleBlur}
-//                   onClick={(e) => e.stopPropagation()}
-//                 />
-//                 {/* Conditions for including a style error */}
-//                 {formik.errors.text && formik.touched.text ? (
-//                   <div className={styles.error}>{formik.errors.text}</div>
-//                 ) : null}
-//               </div>
-//               {/* Buttons */}
-//               <div className={styles.formButton}>
-//                 <button
-//                   className={styles.buttonSubmit}
-//                   type='submit'
-//                   onClick={(e) => e.stopPropagation()}
-//                 >
-//                   <span>ОТПРАВИТЬ</span>
-//                 </button>
-//                 <button
-//                   //*Close form
-//                   className={styles.buttonClose}
-//                   type='button'
-//                 >
-//                   <span>ЗАКРЫТЬ</span>
-//                 </button>
-//               </div>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FormAppointment;
-
-// ! Компонент з відправкою даних форми на пошту
+//*Import base
 import React from 'react';
 import styles from './formAppointment.module.scss';
 import classNames from 'classnames';
+
+//*Import Libraries
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 const FormAppointment = ({ active, setActive }) => {
   const URL = 'https://vetclinic-backend.onrender.com/api/users/send-email';
@@ -176,15 +42,12 @@ const FormAppointment = ({ active, setActive }) => {
 
   const sendData = async (formData) => {
     try {
-      const response = await fetch(URL, {
-        method: 'POST',
+      const response = await axios.post(URL, formData, {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
       });
-      const result = await response.json();
-      console.log(result);
+      console.log(response.data);
     } catch (error) {
       console.error('Error:', error);
     }
--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,198 +1,17 @@
-// //*Registration
-
-// //*Import base
-// import styles from './registration.module.scss';
-// import classNames from 'classnames';
-
-// // *Import Formik library for creating forms
-// import { useFormik } from 'formik';
-
-// // *Import Yup library for validation forms
-// import * as Yup from 'yup';
-
-// const Registration = ({ active, setActive }) => {
-//   const URL = 'https://vetclinic-backend.onrender.com/api/users/registration';
-//   //*Formik
-//   const formik = useFormik({
-//     initialValues: {
-//       firstName: '',
-//       lastName: '',
-//       userName: '',
-//       email: '',
-//       password: '',
-//     },
-//     //*Validation
-//     validationSchema: Yup.object({
-//       firstName: Yup.string()
-//         .min(2, 'Минимум 2 символова')
-//         .required('Обязательное поле!'),
-//       lastName: Yup.string()
-//         .min(2, 'Минимум  2 символов')
-//         .required('Обязательное поле!'),
-//       userName: Yup.string()
-//         .min(4, 'Минимум  4 символов')
-//         .required('Обязательное поле!'),
-//       email: Yup.string()
-//         .email('Неправильный email адрес')
-//         .required('Обязательное поле!'),
-//       password: Yup.string()
-//         .min(6, 'Минимум  6 символов')
-//         .required('Обязательное поле!'),
-//     }),
-//     onSubmit: (values, { setSubmitting, resetForm }) => {
-//       console.log(JSON.stringify(values, null, 2));
-//       setTimeout(() => {
-//         setSubmitting(false);
-//         resetForm();
-//       }, 400);
-//     },
-//   });
-//   //*Sending data form
-//   fetch(URL, {
-//     method: 'POST',
-//     headers: { Registration: 'form registration' },
-//     body: JSON.stringify(formik),
-//   })
-//     .then((response) => response.json())
-//     .then((result) => console.log(result));
-//   return (
-//     <div
-//       //*Сonditions for switching styles in mode active
-//       className={
-//         active ? classNames(styles.modal, styles.active) : styles.modal
-//       }
-//       onClick={() => setActive(false)}
-//     >
-//       {/* Form */}
-//       <form
-//         className={styles.form}
-//         method='post'
-//         onSubmit={formik.handleSubmit}
-//       >
-//         {/* Title form */}
-//         <h2 className={styles.title}>РЕГИСТРАЦИЯ</h2>
-//         <div className={styles.wrapper}>
-//           {/* First name */}
-//           <label className={styles.label} htmlFor='firstName'></label>
-//           <input
-//             className={classNames(styles.input, styles.firstName)}
-//             name='firstName'
-//             placeholder='Имя'
-//             type='text'
-//             value={formik.values.firstName}
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             onClick={(e) => e.stopPropagation()}
-//           />
-//           {/* Conditions for including a style error */}
-//           {formik.errors.firstName && formik.touched.firstName ? (
-//             <div className={styles.error}>{formik.errors.firstName}</div>
-//           ) : null}
-//           {/* Last name */}
-//           <label className={styles.label} htmlFor='lastName'></label>
-//           <input
-//             className={classNames(styles.input, styles.lastName)}
-//             name='lastName'
-//             placeholder='Фамилия'
-//             type='text'
-//             value={formik.values.lastName}
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             onClick={(e) => e.stopPropagation()}
-//           />
-//           {/* Conditions for including a style error */}
-//           {formik.errors.lastName && formik.touched.lastName ? (
-//             <div className={styles.error}>{formik.errors.lastName}</div>
-//           ) : null}
-//           {/* User name */}
-//           <label className={styles.label} htmlFor='userName'></label>
-//           <input
-//             className={classNames(styles.input, styles.userName)}
-//             name='userName'
-//             placeholder='Имя пользователя'
-//             type='text'
-//             value={formik.values.userName}
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             onClick={(e) => e.stopPropagation()}
-//           />
-//           {/* Conditions for including a style error */}
-//           {formik.errors.userName && formik.touched.userName ? (
-//             <div className={styles.error}>{formik.errors.userName}</div>
-//           ) : null}
-//           {/* Email */}
-//           <label className={styles.label} htmlFor='email'></label>
-//           <input
-//             className={classNames(styles.input, styles.email)}
-//             name='email'
-//             placeholder='Почта'
-//             type='email'
-//             value={formik.values.email}
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             onClick={(e) => e.stopPropagation()}
-//           />
-//           {/* Conditions for including a style error */}
-//           {formik.errors.email && formik.touched.email ? (
-//             <div className={styles.error}>{formik.errors.email}</div>
-//           ) : null}
-//           {/* Password */}
-//           <label className={styles.label} htmlFor='password'></label>
-//           <input
-//             className={classNames(styles.input, styles.password)}
-//             name='password'
-//             placeholder='Пароль'
-//             type='password'
-//             value={formik.values.password}
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             onClick={(e) => e.stopPropagation()}
-//           />
-//           {/* Conditions for including a style error */}
-//           {formik.errors.password && formik.touched.password ? (
-//             <div className={styles.error}>{formik.errors.password}</div>
-//           ) : null}
-//           {/* Buttons */}
-//           <div className={styles.button}>
-//             <button
-//               className={styles.buttonSubmit}
-//               type='submit'
-//               onClick={(e) => e.stopPropagation()}
-//             >
-//               <span>Регистрация</span>
-//             </button>
-//             <button
-//               //*Close form
-//               className={styles.buttonClose}
-//               type='button'
-//             >
-//               <span>ЗАКРЫТЬ </span>
-//             </button>
-//           </div>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Registration;
-
 //*Registration
 
 //*Import base
 import styles from './registration.module.scss';
 import classNames from 'classnames';
 
-// *Import Formik library for creating forms
+//*Import Libraries
 import { useFormik } from 'formik';
-
-// *Import Yup library for validation forms
 import * as Yup from 'yup';
+import axios from 'axios';
 
 const Registration = ({ active, setActive }) => {
   const URL = 'https://vetclinic-backend.onrender.com/api/users/registration';
 
-  //*Formik
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -201,23 +20,22 @@ const Registration = ({ active, setActive }) => {
       email: '',
       password: '',
     },
-    //*Validation
     validationSchema: Yup.object({
       firstName: Yup.string()
-        .min(2, 'Минимум 2 символова')
-        .required('Обязательное поле!'),
+        .min(2, 'Мінімум 2 символи')
+        .required("Обов'язкове поле!"),
       lastName: Yup.string()
-        .min(2, 'Минимум  2 символов')
-        .required('Обязательное поле!'),
+        .min(2, 'Мінімум 2 символи')
+        .required("Обов'язкове поле!"),
       userName: Yup.string()
-        .min(4, 'Минимум  4 символов')
-        .required('Обязательное поле!'),
+        .min(4, 'Мінімум 4 символи')
+        .required("Обов'язкове поле!"),
       email: Yup.string()
-        .email('Неправильный email адрес')
-        .required('Обязательное поле!'),
+        .email('Неправильний адрес електронної пошти')
+        .required("Обов'язкове поле!"),
       password: Yup.string()
-        .min(6, 'Минимум  6 символов')
-        .required('Обязательное поле!'),
+        .min(6, 'Мінімум 6 символів')
+        .required("Обов'язкове поле!"),
     }),
     onSubmit: (values, { setSubmitting, resetForm }) => {
       console.log(JSON.stringify(values, null, 2));
@@ -229,103 +47,85 @@ const Registration = ({ active, setActive }) => {
     },
   });
 
-  //*Sending data form
   const sendData = async (formData) => {
     try {
-      const response = await fetch(URL, {
-        method: 'POST',
-        headers: { Registration: 'form registration' },
-        body: JSON.stringify(formData),
-      });
-      const result = await response.json();
-      console.log(result);
+      const response = await axios.post(URL, formData);
+      console.log(response.data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Помилка:', error);
     }
   };
 
   return (
     <div
-      //*Сonditions for switching styles in mode active
       className={
         active ? classNames(styles.modal, styles.active) : styles.modal
       }
       onClick={() => setActive(false)}
     >
-      {/* Form */}
       <form
         className={styles.form}
         method='post'
         onSubmit={formik.handleSubmit}
       >
-        {/* Title form */}
-        <h2 className={styles.title}>РЕГИСТРАЦИЯ</h2>
+        <h2 className={styles.title}>РЕЄСТРАЦІЯ</h2>
         <div className={styles.wrapper}>
-          {/* First name */}
           <label className={styles.label} htmlFor='firstName'></label>
           <input
             className={classNames(styles.input, styles.firstName)}
             name='firstName'
-            placeholder='Имя'
+            placeholder="Ім'я"
             type='text'
             value={formik.values.firstName}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             onClick={(e) => e.stopPropagation()}
           />
-          {/* Conditions for including a style error */}
           {formik.errors.firstName && formik.touched.firstName ? (
             <div className={styles.error}>{formik.errors.firstName}</div>
           ) : null}
-          {/* Last name */}
           <label className={styles.label} htmlFor='lastName'></label>
           <input
             className={classNames(styles.input, styles.lastName)}
             name='lastName'
-            placeholder='Фамилия'
+            placeholder='Прізвище'
             type='text'
             value={formik.values.lastName}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             onClick={(e) => e.stopPropagation()}
           />
-          {/* Conditions for including a style error */}
           {formik.errors.lastName && formik.touched.lastName ? (
             <div className={styles.error}>{formik.errors.lastName}</div>
           ) : null}
-          {/* User name */}
           <label className={styles.label} htmlFor='userName'></label>
           <input
             className={classNames(styles.input, styles.userName)}
             name='userName'
-            placeholder='Имя пользователя'
+            placeholder="Ім'я користувача"
             type='text'
             value={formik.values.userName}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             onClick={(e) => e.stopPropagation()}
           />
-          {/* Conditions for including a style error */}
           {formik.errors.userName && formik.touched.userName ? (
             <div className={styles.error}>{formik.errors.userName}</div>
           ) : null}
-          {/* Email */}
           <label className={styles.label} htmlFor='email'></label>
           <input
             className={classNames(styles.input, styles.email)}
             name='email'
-            placeholder='Почта'
+            placeholder='Електронна пошта'
             type='email'
             value={formik.values.email}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             onClick={(e) => e.stopPropagation()}
           />
-          {/* Conditions for including a style error */}
           {formik.errors.email && formik.touched.email ? (
             <div className={styles.error}>{formik.errors.email}</div>
           ) : null}
-          {/* Password */}
           <label className={styles.label} htmlFor='password'></label>
           <input
             className={classNames(styles.input, styles.password)}
@@ -337,25 +137,19 @@ const Registration = ({ active, setActive }) => {
             onBlur={formik.handleBlur}
             onClick={(e) => e.stopPropagation()}
           />
-          {/* Conditions for including a style error */}
           {formik.errors.password && formik.touched.password ? (
             <div className={styles.error}>{formik.errors.password}</div>
           ) : null}
-          {/* Buttons */}
           <div className={styles.button}>
             <button
               className={styles.buttonSubmit}
               type='submit'
               onClick={(e) => e.stopPropagation()}
             >
-              <span>Регистрация</span>
+              <span>Реєстрація</span>
             </button>
-            <button
-              //*Close form
-              className={styles.buttonClose}
-              type='button'
-            >
-              <span>ЗАКРЫТЬ </span>
+            <button className={styles.buttonClose} type='button'>
+              <span>ЗАКРЫТЬ</span>
             </button>
           </div>
         </div>
