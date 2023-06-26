6776f0ede484e45faca63a404217d38951755200
Skubenyk <skubenikl@gmail.com>
2023-06-26 16:55:35 +0300

    changing links

---
+++ build/static/js/827.419c8bb0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkVetClinic=self.webpackChunkVetClinic||[]).push([[827],{6827:function(e,n,s){s.r(n);var r=s(7326),t=s(1694),c=s.n(t),a=s(184);n.default=function(e){var n=e.active,s=e.setActive;return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("div",{className:r.Z.headerBurger,children:(0,a.jsxs)("button",{onClick:function(){return s(!n)},className:n?c()(r.Z.burger,r.Z.show):r.Z.burger,type:"button",children:[(0,a.jsx)("span",{}),(0,a.jsx)("span",{}),(0,a.jsx)("span",{})]})})})}}}]);
+//# sourceMappingURL=827.419c8bb0.chunk.js.map
